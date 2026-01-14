19b0c7bbe11f2a86b48a30b71bfb26c13444eb68
Fastestkyo <fastestkyo@gmail.com>
2026-01-14 21:57:48 +0530

    asd

---
+++ plr/shlogoon.gd
@@ -0,0 +1,28 @@
+extends StaticBody2D
+var rand
+const JACK_SOUNDS = [
+preload("res://assets/plr/shlogooner/increaseaudio/jack1 (mp3cut.net).mp3"), preload("res://assets/plr/shlogooner/increaseaudio/jack2 (mp3cut.net).mp3"), preload("res://assets/plr/shlogooner/increaseaudio/jack3 (mp3cut.net).mp3"), preload("res://assets/plr/shlogooner/increaseaudio/jack4 (mp3cut.net).mp3"), preload("res://assets/plr/shlogooner/increaseaudio/jack5 (mp3cut.net).mp3"), preload("res://assets/plr/shlogooner/increaseaudio/jack6 (mp3cut.net).mp3")
+]
+func _ready() -> void:
+	randoma()
+
+func get_dmged(_Dmg):
+	sound()
+
+func sound():
+	var sound = JACK_SOUNDS.pick_random()
+	$AudioStreamPlayer2D.stream = sound
+	$AudioStreamPlayer2D.play()
+	randoma()
+
+func knockback(_a,_b):
+	pass
+
+func randoma():
+	rand = randi()
+	if rand % 2 == 0:
+		$Deepfried1768405469836.visible = true
+		$Deepfried1768405484777.visible = false
+	else:
+		$Deepfried1768405484777.visible = true
+		$Deepfried1768405469836.visible = false
